f35c8ee3f7604e187146291325418114c2dcd16d
Dominik Winter <dominik@sydev.de>
2023-04-20 13:01:15 +0200

    refactor(Browser): Remove brackets

--- 02-browser/build.sh
+++ 02-browser/build.sh
@@ -1,3 +1,3 @@
 rm -rf dist && mkdir dist #&& cp src/index.html dist/index.html
 
-docker run --rm  -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk emcc src/main.c -o dist/index.html --shell-file src/index.html -s NO_EXIT_RUNTIME=1 -s EXPORTED_RUNTIME_METHODS=[ccall]
+docker run --rm  -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk emcc src/main.c -o dist/index.html --shell-file src/index.html -s NO_EXIT_RUNTIME=1 -s EXPORTED_RUNTIME_METHODS=ccall
