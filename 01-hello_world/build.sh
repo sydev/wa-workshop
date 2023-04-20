d9f01eff69d2cbfff1c8b5cb25db11830be64f98
Dominik Winter <dominik@sydev.de>
2023-04-20 12:38:35 +0200

    chore(): CPP to C

--- 01-hello_world/build.sh
+++ 01-hello_world/build.sh
@@ -1,3 +1,3 @@
 mkdir dist
 
-docker run --rm  -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk emcc src/main.cpp -o dist/index.js
+docker run --rm  -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk emcc src/main.c -o dist/index.js
